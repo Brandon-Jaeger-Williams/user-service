0d6b9e1ee5c6d58230cf1758732a39e57c04a2aa
brandonwilliams <brandonjaegerwilliams@gmail.com>
2024-07-11 23:36:53 +0200

    feature: added script to insert users and added role to user

--- src/main/java/com/assessment/userservice/entity/UserEntity.java
+++ src/main/java/com/assessment/userservice/entity/UserEntity.java
@@ -1,18 +1,21 @@
 package com.assessment.userservice.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.validation.constraints.Size;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
 @Table(name = "users")
 @EqualsAndHashCode(callSuper = true)
-public class UserEntity extends AuditEntity {
+public class UserEntity extends AuditEntity implements UserDetails {
 
     @Column(name = "username")
     @Size(max = 50, message = "The maximum length of username is 50")
@@ -21,4 +24,38 @@ public class UserEntity extends AuditEntity {
     @Column(name = "password")
     @Size(max = 255, message = "The maximum length of password is 255")
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    private Role role;
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
